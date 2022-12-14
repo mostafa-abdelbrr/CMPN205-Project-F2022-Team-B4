9c9ea99692573fce1a0f6fc47fe5011bea6eb014
PowerOfMagic <111122263+PowerOfMagic@users.noreply.github.com>
2022-12-14 20:25:04 +0200

    Transform-Pipeline-mesh

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,44 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
+
+            { if (faceCulling.enabled ==true)
+            {
+                glEnable(GL_CULL_FACE);
+                glCullFace(faceCulling.culledFace);
+                glFrontFace(faceCulling.frontFace);
+            } 
+            else
+            {
+                glDisable(GL_CULL_FACE);
+            }
+
+            if (depthTesting.enabled== true)
+            {
+                glEnable(GL_DEPTH_TEST);
+                glDepthFunc(depthTesting.function);
+            }
+            else
+            {
+                glDisable(GL_DEPTH_TEST);
+            }
+
+            if (blending.enabled== true)
+            {
+                glEnable(GL_BLEND);
+                glBlendEquation(blending.equation);
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                glBlendColor(blending.constantColor.x, blending.constantColor.y, blending.constantColor.z, blending.constantColor.w);
+            }
+            else
+            {
+                glDisable(GL_BLEND);
+
+            }
+                       
+            glColorMask(colorMask.x, colorMask.y, colorMask.z, colorMask.w);
+            glDepthMask(depthMask);  
+            }
         }
 
         // Given a json object, this function deserializes a PipelineState structure
--- source/common/mesh/mesh.hpp
+++ source/common/mesh/mesh.hpp
@@ -17,6 +17,7 @@ namespace our {
         unsigned int VAO;
         // We need to remember the number of elements that will be draw by glDrawElements 
         GLsizei elementCount;
+        GLsizei VertxCount;
     public:
 
         // The constructor takes two vectors:
@@ -30,6 +31,57 @@ namespace our {
         {
             //TODO: (Req 2) Write this function
             // remember to store the number of elements in "elementCount" since you will need it for drawing
+            elementCount=elements.size();
+
+            //Generate Vertex Array and Bind inorder to use it
+            glGenVertexArrays(1,&VAO);
+            glBindVertexArray(VAO);
+
+            //Create Vertex Buffer and bind  inorder to fill it
+            glGenBuffers(1,&VBO);
+            glBindBuffer(GL_ARRAY_BUFFER,VBO);
+            // Now lets fill Buffer
+            VertxCount=vertices.size();
+            glBufferData(GL_ARRAY_BUFFER,VertxCount*sizeof(Vertex),&vertices[0],GL_STATIC_DRAW);
+
+            //Create ELement Buffer
+            glGenBuffers(1,&EBO);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER,elementCount*sizeof(unsigned int),&elements[0],GL_STATIC_DRAW);
+             // Now utilizing the Vertex array to work on Buffers
+
+             //Attributes 
+
+             //position Attribute
+             glEnableVertexAttribArray(ATTRIB_LOC_POSITION); //Enable it
+             //abd2 22ra mn Position
+             glVertexAttribPointer(ATTRIB_LOC_POSITION,3,GL_FLOAT,false,sizeof(Vertex),(void*)offsetof(Vertex,position));
+
+             // Apply on other attributes
+             glEnableVertexAttribArray(ATTRIB_LOC_COLOR);
+
+             glVertexAttribPointer(ATTRIB_LOC_COLOR,4,GL_UNSIGNED_BYTE,true,sizeof(Vertex),(void*)offsetof(Vertex,color));
+
+
+
+             glEnableVertexAttribArray(ATTRIB_LOC_NORMAL);
+
+             glVertexAttribPointer(ATTRIB_LOC_NORMAL,3,GL_FLOAT,false,sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+             
+             glEnableVertexAttribArray(ATTRIB_LOC_TEXCOORD);
+
+             glVertexAttribPointer(ATTRIB_LOC_TEXCOORD,2,GL_FLOAT,false,sizeof(Vertex), (void*)offsetof(Vertex, tex_coord));
+
+             
+             
+
+
+          
+
+
+
+
             // For the attribute locations, use the constants defined above: ATTRIB_LOC_POSITION, ATTRIB_LOC_COLOR, etc
             
         }
@@ -38,11 +90,17 @@ namespace our {
         void draw() 
         {
             //TODO: (Req 2) Write this function
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, elementCount,GL_UNSIGNED_INT,(void*)0);
+
         }
 
         // this function should delete the vertex & element buffers and the vertex array object
         ~Mesh(){
             //TODO: (Req 2) Write this function
+            glDeleteVertexArrays(1,&VBO);
+            glDeleteVertexArrays(1,&VAO);
+            glDeleteVertexArrays(1,&EBO);
         }
 
         Mesh(Mesh const &) = delete;
