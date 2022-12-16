43133cc9aaa07b8bdf851ae65adfb630bfbe07b1
Mostafa Abdelbrr <mostafa.abdelbrr@hotmail.com>
2022-12-16 20:16:54 +0200

    Reset edits to sampler.

--- source/common/texture/sampler.hpp
+++ source/common/texture/sampler.hpp
@@ -26,12 +26,7 @@ namespace our {
         // This method binds this sampler to the given texture unit
         void bind(GLuint textureUnit) const {
             //TODO: (Req 6) Complete this function
-            if (this) {
-                glBindSampler(textureUnit,name);
-            }
-            else {
-                glBindSampler(textureUnit, 0);
-            }
+            glBindSampler(textureUnit,name);
         }
 
         // This static method ensures that no sampler is bound to the given texture unit
